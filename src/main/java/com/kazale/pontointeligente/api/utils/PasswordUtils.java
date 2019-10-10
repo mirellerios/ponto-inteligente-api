a95c90f8f4f20934949fed0ae138ed8c3b7b2807
Mirelle De Melo Rios Silva <mirelle.silva@AD002.NET02>
2019-10-10 08:58:16 -0300

    Adicionado novos arquivos ao projeto

---
+++ src/main/java/com/kazale/pontointeligente/api/utils/PasswordUtils.java
@@ -0,0 +1,36 @@
+package com.kazale.pontointeligente.api.utils;
+import java.util.logging.Logger;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+
+	
+	/**
+	* Gera um hash utilizando o BCrypt.
+	*
+	* @param senha
+	* @return String
+	*/
+	public static String gerarBCrypt ( String senha ) {
+		if ( senha == null ) {
+				return senha ;
+		}
+		
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder ();
+		return bCryptEncoder.encode ( senha );
+	}
+	
+	/**
+	* Verifica se a senha é válida.
+	*
+	* @param senha
+	* @param senhaEncoded
+	* @return boolean
+	*/
+	public static boolean senhaValida ( String senha , String senhaEncoded ) {
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder ();
+		return bCryptEncoder . matches ( senha , senhaEncoded );
+	}
+	
+}
